7a5d556f73c332c1992065b452291ef78810eea1
dbhinton <dhinton.us@gmail.com>
2023-03-20 11:29:17 -0400

    update

---
+++ frontend/src/components/OrderPickupTimeScatterplot.tsx
@@ -0,0 +1,48 @@
+import React, { useState, useEffect } from 'react';
+import Plot from 'react-plotly.js';
+import axios from 'axios';
+
+interface ScatterplotData {
+  x: number[],
+  y: number[],
+}
+
+const OrderPickupTimeScatterplot = () => {
+  const [scatterplotData, setScatterplotData] = useState<ScatterplotData>({ x: [], y: [] });
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await axios.get('http://localhost:8000/order-pickup-scatterplot/');
+      setScatterplotData(response.data);
+    };
+
+    fetchData();
+  }, []);
+
+  return (
+    <Plot
+      data={[
+        {
+          x: scatterplotData.x,
+          y: scatterplotData.y,
+          mode: 'markers',
+          marker: { color: 'blue', size: 10 },
+          type: 'scatter',
+        },
+      ]}
+      layout={{
+        width: 800,
+        height: 600,
+        xaxis: {
+          title: 'Pickup Time',
+        },
+        yaxis: {
+          title: 'Order Time',
+        },
+        title: 'Order Time vs Pickup Time Scatterplot',
+      }}
+    />
+  );
+};
+
+export default OrderPickupTimeScatterplot;
